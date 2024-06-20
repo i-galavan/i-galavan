9b0b255fd67dad42a959ade7405a69f1b936f108
i-galavan <ionut.galavan@gmail.com>
2024-06-20 12:53:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>John Doe</h1>
-        <p>Full Stack Developer | Tech Enthusiast | Blogger</p>
+        <h1>Ionut Galavan</h1>
+        <p> Tech Enthusiast </p>
       </header>
       
       <section className="about">
         <h2>About Me</h2>
         <p>
-          I am a passionate developer with a love for creating dynamic and responsive web applications. With a background in both front-end and back-end development, I enjoy working on projects that require thoughtful planning and creative problem-solving.
+          I enjoy working on projects that require thoughtful planning and creative problem-solving.
         </p>
       </section>
       
@@ -27,23 +27,23 @@ function App() {
         </ul>
       </section>
       
-      <section className="projects">
-        <h2>Projects</h2>
-        <div className="project-card">
-          <h3>Project One</h3>
-          <p>A web application for tracking personal expenses.</p>
-        </div>
-        <div className="project-card">
-          <h3>Project Two</h3>
-          <p>A social media platform for connecting with friends.</p>
-        </div>
-      </section>
+      // <section className="projects">
+      //   <h2>Projects</h2>
+      //   <div className="project-card">
+      //     <h3>Project One</h3>
+      //     <p>A web application for tracking personal expenses.</p>
+      //   </div>
+      //   <div className="project-card">
+      //     <h3>Project Two</h3>
+      //     <p>A social media platform for connecting with friends.</p>
+      //   </div>
+      // </section>
       
       <section className="contact">
         <h2>Contact</h2>
-        <p>Email: johndoe@example.com</p>
-        <p>LinkedIn: linkedin.com/in/johndoe</p>
-        <p>GitHub: github.com/johndoe</p>
+        <p>Email: ionut.galavan@gmail.com</p>
+
+        <p>GitHub: github.com/i-galavan</p>
       </section>
     </div>
   );
